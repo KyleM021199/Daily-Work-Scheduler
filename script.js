c59489c3717226afd0f1ae2d4558aa311a180cb3
mcclendokj11 <118197174+KyleM021199@users.noreply.github.com>
2023-04-01 19:11:25 -0500

    Added current date

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveButton = $('.saveBtn');
+var hour = 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +10,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -15,9 +18,30 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
   // TODO: Add code to display the current date in the header of the page.
+//  var addOrdinal = (num) => {
+//     switch(num){
+//         case 1:
+//             return num + 'st'
+//         break
+//         case 2:
+//             return num + 'nd'
+//         break
+//         case 3:
+//             return num + 'rd'
+//         break
+//         default:
+//             return num + 'th'
+//     }
+// }
+  var today = dayjs();
+  var currentDate = today.format('dddd, MMMM D');
+  $('#currentDay').text(currentDate);
 });
